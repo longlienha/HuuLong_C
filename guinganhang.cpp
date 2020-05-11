8b7ca50dfb5969ea70898ebd36df6888c7b4ef48
longlienha <longlienha20@gmail.com>
2020-05-11 13:03:09 +0700

    Asignment 3

---
+++ guinganhang.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	float money=4000;
+	
+	money=money + money*8/100;
+	money=money + money*8/100;
+	money=money + money*8/100;
+	money=money + money*8/100;
+	
+	printf("tong tien sau 4 nam gui la: %f",money);
+}
