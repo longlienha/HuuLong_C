1e6ef11f0d4ec4d63025dd0e7a23357b03b0d7a5
longlienha <longlienha20@gmail.com>
2020-05-12 20:43:56 +0700

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -6,16 +6,9 @@ int main(){
 	printf("nhap vao so bat ki: ");
 	scanf("%d", &a);
 	int a1=a,a2=0;
-	if(a>0){
-		for(int i=0;a1>0;i++){
-			a2=a2*10+a1%10;
-			a1/=10;
-		}
-	}else{
-		for(int i=0;a1<0;i++){
-			a2=a2*10+a1%10;
-			a1/=10;
-		}
+	for(int i=0;a1!=0;i++){
+		a2=a2*10+a1%10;
+		a1/=10;
 	}
 	
 	printf("so nguoc la: %d", a2);
