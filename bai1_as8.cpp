df6109ffa477afec8e1536c11ee53b991af0d9a6
longlienha <longlienha20@gmail.com>
2020-05-22 09:32:23 +0700

    Assignment 8

---
+++ bai1_as8.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n=0,j=0;
+	printf("nhap vao n= ");
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		printf("nhap vao phan tu thu %d: ",i);
+		scanf("%d", &arr[i]);
+	}
+	
+	int *p;
+	p=(int *)calloc(1,sizeof(int));
+	for(int dem=0,i=0;i<n;i++){
+		if(arr[i]>=0){
+			dem++;
+		}else{
+			*(p+j)=dem;
+			dem=0;
+			p=(int *)realloc(p,1*sizeof(int));
+			j++;
+		}
+	}
+	
+	int max=*(p+0);
+	for(int i=1;i<=j;i++){
+		if(max < *(p+i)){
+			max=*(p+i);
+		}
+	}
+	
+	printf("so so duong lien tiep: %d",max);
+}
---
+++ bai2.1_as8.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n=0,j=0;
+	printf("nhap vao n= ");
+	scanf("%d",&n);
+	int *p1;
+	p1=(int *)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++){
+		printf("nhap vao phan tu thu %d: ",i);
+		scanf("%d", (p1+i));
+	}
+	
+	int *p;
+	p=(int *)calloc(1,sizeof(int));
+	for(int sum=0,i=0;i<n;i++){
+		if(*(p1+i)>=0){
+			sum=sum+*(p1+i);
+		}else{
+			*(p+j)=sum;
+			sum=0;
+			p=(int *)realloc(p,1*sizeof(int));
+			j++;
+		}
+	}
+	
+	int max=*(p+0);
+	for(int i=1;i<=j;i++){
+		if(max < *(p+i)){
+			max=*(p+i);
+		}
+	}
+	
+	printf("tong so duong lien tiep lon nhat: %d",max);
+}
